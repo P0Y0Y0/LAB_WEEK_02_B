cc663e09fece804fdad95abd1230a27debb3c6d8
P0Y0Y0 <erufumang@gmail.com>
2025-09-11 00:29:27 +0700

    Commit No. 3 - Fix application crash when user input wrong color code [Intent]

--- app/src/main/java/com/example/lab_week_02_b/ResultActivity.kt
+++ app/src/main/java/com/example/lab_week_02_b/ResultActivity.kt
@@ -1,5 +1,6 @@
 package com.example.lab_week_02_b
 
+import android.content.Intent
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.widget.TextView
@@ -10,10 +11,12 @@ import androidx.constraintlayout.widget.ConstraintSet
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import android.graphics.Color
+import android.app.Activity
 
 class ResultActivity : AppCompatActivity() {
     companion object {
         private const val COLOR_KEY = "COLOR_KEY"
+        private const val ERROR_KEY = "ERROR_KEY"
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,7 +28,17 @@ class ResultActivity : AppCompatActivity() {
 
             val backgroundScreen =
                 findViewById<ConstraintLayout>(R.id.background_screen)
+            try {
                 backgroundScreen.setBackgroundColor(Color.parseColor("#$colorCode"))
+            }
+            catch (ex: IllegalArgumentException) {
+                Intent().let {
+                    errorIntent ->
+                    errorIntent.putExtra(ERROR_KEY, true)
+                    setResult(Activity.RESULT_OK, errorIntent)
+                    finish()
+                }
+            }
 
             val resultMessage =
                 findViewById<TextView>(R.id.color_code_result_message)
